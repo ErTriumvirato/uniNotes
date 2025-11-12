9b3167dbbf579c974608787893f7d836168e13f2
Alessandro Testa <88205550+ale-423@users.noreply.github.com>
2025-11-12 12:45:03 +0100

    test

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>uniNotes</title>
+</head>
+<body>
+    <h2>Our website is currently under maintenance. We'll be back online soon. Thank you for your patience!</h2>
+</body>
+</html>
