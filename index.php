f01b54b2b2341bb1ef021fe233b855b43ba8ccfe
ale-423 <88205550+ale-423@users.noreply.github.com>
2025-11-27 09:59:16 +0100

    Add base.php and initial menu structure

--- index.php
+++ index.php
@@ -1,11 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+require 'templates/base.php';
+?>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>uniNotes</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
 </head>
-<body>
-    <h2>Our website is currently under maintenance. We'll be back online soon. Thank you for your patience!</h2>
-</body>
-</html>
+<!-- <h1 class="text-center fw-bold my-5 position-relative">
+        Ultimi appunti caricati
+    </h1>
+
+    <main>
+        <article class="card shadow-sm border-0 my-4" style="max-width: 600px; margin: auto;">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShU27oapa9avSl-LuQEQWYWbIdFFJMJfn6XA&s"
+                class="card-img-top"
+                alt="Immagine articolo">
+
+            <div class="card-body text-center">
+                <h2 class="card-title mb-3">Articolo</h2>
+
+                <p class="text-muted mb-3">
+                    Autore: Mario Rossi &nbsp; | &nbsp; 25 Nov 2025
+                </p>
+
+                <a href="#" class="btn btn-primary px-4">
+                    Leggi tutto
+                </a>
+            </div>
+        </article>
+    </main>
+
+    <footer class="bg-dark text-light pt-4 pb-3 mt-5">
+        <div class="container text-center">
+            <p class="mb-1">Paolo Foschini, Alessandro Testa, Ciro Bassi</p>
+        </div>
+    </footer>
+    -->
---
+++ templates/base.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>uniNotes</title>
+    <link rel="icon" href="uploads/img/favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg custom-navbar px-3">
+            <a class="navbar-brand d-flex align-items-center" href="#">
+                <img src="uploads/img/logo.png" alt="Logo" />
+            </a>
+
+            <div class="d-flex d-lg-none ms-auto align-items-center">
+                <a href="#" class="btn btn-primary me-2">Login</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mx-lg-auto text-center">
+                    <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Servizi</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Contatti</a></li>
+                </ul>
+                <a href="#" class="btn btn-primary d-none d-lg-block">Login</a>
+            </div>
+        </nav>
+    </header>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
