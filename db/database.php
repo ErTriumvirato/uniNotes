f002c28d6a0c8b3f16aaeb061518fcc9b9e876ed
Paolo Foschini <paolo.foschini2@studio.unibo.it>
2025-12-01 09:50:56 +0100

    corsi page implemented

--- db/database.php
+++ db/database.php
@@ -22,9 +22,24 @@ class DatabaseHelper
     }
 
     public function getCoursesWithSSD(){       
-        $query = "SELECT corsi.nome AS nomeCorso, ssd.nome AS nomeSSD, corsi.descrizione AS descrizioneCorso 
+        $query = "SELECT corsi.nome AS nomeCorso, ssd.nome AS nomeSSD, corsi.descrizione AS descrizioneCorso
         FROM corsi JOIN ssd ON corsi.idssd = ssd.idssd";
         $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+    
+    public function getFollowedCoursesWithSSD($userId){
+        $query = "SELECT corsi.nome AS nomeCorso, ssd.nome AS nomeSSD, corsi.descrizione AS descrizioneCorso
+        FROM corsi 
+        JOIN ssd ON corsi.idssd = ssd.idssd
+        JOIN iscrizioni ON corsi.idcorso = iscrizioni.idcorso
+        WHERE iscrizioni.idutente = ?";
+        $stmt = $this->db->prepare($query);
+        // Assuming $userId is available in the context where this method is called
+        $stmt->bind_param('i', $userId);
         $stmt->execute();
         $result = $stmt->get_result();
 
--- templates/lista-corsi.php
+++ templates/lista-corsi.php
@@ -1,10 +1,58 @@
 <?php
     require_once 'db/database.php';
     require_once 'config.php';
-    
-    $lista = $dbh->getCoursesWithSSD();
-    foreach($lista as $corso){
-        echo $corso['nomeCorso'] . " (" . $corso['nomeSSD'] . ")" . "<br>" . $corso['descrizioneCorso'] . "<br>";
-    }
 ?>
-    
+
+<h1 class="text-center fw-bold my-5 position-relative">
+    Corsi Seguiti
+</h1>
+
+<?php
+    $userId = 2; // esempio, da prendere successivamente al login
+    $seguiti = $dbh->getFollowedCoursesWithSSD($userId);
+    foreach ($seguiti as $corsoseguito) :
+?>
+<article class="card shadow-sm border-0 my-4" style="max-width: 600px; margin: auto;">
+
+    <a href="corso.php">
+    <div class="card-body text-center">
+        <h2 class="card-title mb-3"><?php echo $corsoseguito['nomeCorso'] ?></h2>
+
+        <p class="text-muted mb-3">
+            <?php echo $corsoseguito['descrizioneCorso'] ?>
+        </p>
+
+    </div>
+    </a>
+</article>
+<?php
+    endforeach;
+?>
+
+<h1 class="text-center fw-bold my-5 position-relative">
+    Altri Corsi
+</h1>
+
+<?php
+    $nonSeguiti = $dbh->getCoursesWithSSD();
+    foreach ($nonSeguiti as $corso) :
+        if (in_array($corso, $seguiti)) {
+            continue;
+        }
+?>
+<article class="card shadow-sm border-0 my-4" style="max-width: 600px; margin: auto;">
+
+    <a href="corso.php">
+    <div class="card-body text-center">
+        <h2 class="card-title mb-3"><?php echo $corso['nomeCorso'] ?></h2>
+
+        <p class="text-muted mb-3">
+            <?php echo $corso['descrizioneCorso'] ?>
+        </p>
+
+    </div>
+    </a>
+</article>
+<?php
+    endforeach;
+?>
