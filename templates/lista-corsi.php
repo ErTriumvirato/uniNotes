6bd1ddd55007d6ec3922f65eeb8325ec2bda676f
PaoloFoschini <pol.foschini@gmail.com>
2025-12-03 10:03:45 +0100

    added follow/unfollow images

--- templates/lista-corsi.php
+++ templates/lista-corsi.php
@@ -7,11 +7,11 @@ foreach ($corsi as $corso) :
 ?>
 
 <article class="card shadow-sm border-0 my-4" style="max-width: 600px; margin: auto;">
-
+    <img src="uploads/img/follow.svg" class="card-img-top" alt="Immagine corso">
     <a href="corso.php">
     <div class="card-body text-center">
         <h2 class="card-title mb-3"><?php echo $corso['nomeCorso'] ?></h2>
-
+        
         <p class="text-muted mb-3">
             <?php echo $corso['descrizioneCorso'] ?>
         </p>
