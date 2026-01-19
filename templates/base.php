278385050945b14eb4b18a707391ebae11338950
Alessandro Testa <88205550+ale-423@users.noreply.github.com>
2026-01-19 15:18:13 +0100

    refactoring
    Co-authored-by: Paolo Foschini <PaoloFoschini@users.noreply.github.com>
    Co-authored-by: Ciro Bassi <cirobassi11@users.noreply.github.com>

--- templates/base.php
+++ templates/base.php
@@ -9,8 +9,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="css/style.css" />
     <link rel="icon" href="uploads/img/favicon.png" type="image/x-icon" />
-    <script src="utils/functions.js"></script>
-    <script type="text/javascript" id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js"></script>
 </head>
 
 <body class="d-flex flex-column min-vh-100">
@@ -133,6 +131,8 @@
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="utils/functions.js"></script>
+    <script type="text/javascript" id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js"></script>
     <script>
         // Funzione generica per mostrare un banner
         function showBanner(message, type) {
